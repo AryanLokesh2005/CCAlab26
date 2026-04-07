365db21b2b93b59eb3c5c8b03762e0b1798e7263
AryanLokesh2005 <aryanlokesh265@gmail.com>
2026-04-07 09:40:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import flask, render_template
-app = Flask(_name_)
-@app.rout('/')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
 def home():
-  # render_template automatically looks in the/ templates folder
-  return render_template('index.html', title = "Home Page")
-if __name__ == '__main__';
-#Locel development server
-app.run(host = '127.0.01', port = 8080, debug = True)
+    # render_template automatically looks in the /templates folder
+    return render_template('index.html', title="Home Page")
+
+if __name__ == '__main__':
+    # Local development server
+    app.run(host='127.0.0.1', port=8080, debug=True)
