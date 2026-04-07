a9fa3355e6d5c1f03a22a1a09c0c66ab8966c878
AryanLokesh2005 <aryanlokesh265@gmail.com>
2026-04-07 08:45:16 +0530

    Initialize Flask app with home route
    Fix syntax errors and set up Flask app.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+from flask import flask, render_template
+app = Flask(_name_)
+@app.rout('/')
+def home():
+  # reender_template automatically looks in the/ templates folder
+  return render_template('index.html', title = "Home Page")
+if __name__ == '__main__';
+#Locel development server
+app.run(host = '127.0.01', port = 8080, debug = True)
